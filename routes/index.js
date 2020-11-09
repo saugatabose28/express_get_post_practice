f6e130b958c5b16794388e0081521a578910c826
Saugata Bose <testcaseexam28@gmail.com>
2020-11-10 03:26:36 +1100

    Delete index.js

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-let landing=require('../controllers/landing');
-/* GET home page. */
-router.get('/', landing.get_landing);
-/* POST email and return to home page */
-router.post('/',landing.submit_lead);
-module.exports = router;
